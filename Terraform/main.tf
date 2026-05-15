d79f7683a3e1e42a0e4f1e755309d9c295371897
DattasaiBattu1999 <battudattasai99@gmail.com>
2026-05-15 17:04:27 +0530

    Module main.tf updated

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,15 +1,16 @@
-provider "aws" {
-  region = "ap-southeast-1"
-}
+# Root Terraform/main.tf
 
-# Module for Networking
 module "network" {
   source = "./VPC"
 }
 
-# Module for Kubernetes Cluster
 module "kubernetes" {
-  source     = "./EKS"
-  vpc_id     = module.network.vpc_id
-  subnet_ids = module.network.public_subnets
+  source = "./EKS"
+  
+  # Map the output "private_subnet_ids" from VPC to the EKS module
+  private_subnet_ids = module.network.private_subnet_ids
+  
+  # If your EKS module needs the VPC ID, make sure it is defined 
+  # in EKS/variables.tf first, otherwise remove this line.
+  # vpc_id = module.network.vpc_id 
 }
